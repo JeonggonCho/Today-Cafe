a98c3c288c5801e6dfb3aaa89e620e70158d09c1
jeonseongjae98 <jeonseongjae@naver.com>
2023-05-03 18:25:38 +0900

    Add profile_image By BE_team

--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+
 class User(AbstractUser):
+    profile_image = models.ImageField(blank=True, null=True, upload_to='images/profile/')
     followings = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+
--- project/urls.py
+++ project/urls.py
@@ -17,8 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
+
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('posts/', include('posts.urls')),
