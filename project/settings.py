c63eb94648f9ada8e105a6e5038eecf25da70d43
JeonggonCho <wjdrhs62@gmail.com>
2023-05-08 05:20:42 +0900

    deployment

--- project/settings.py
+++ project/settings.py
@@ -20,12 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=vqx&kz0)0k0$$c)hcr4ni-4zk=0iirm4rh65pw-4*r)a49z3c'
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-todaycafe-e9btb72blgn9ob90.sel3.cloudtype.app']
 
 
 # Application definition
