609ab040671eb18a5d077b803624c366a13f0b23
JeonggonCho <wjdrhs62@gmail.com>
2023-05-08 05:32:43 +0900

    re deployment

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['port-0-todaycafe-e9btb72blgn9ob90.sel3.cloudtype.app']
 
 
 # Application definition
